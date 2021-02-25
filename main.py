7a22fb346f34d1ecaf3ea89f6063cf263a4e9394
Puzanovim <puzanovim@yandex.ru>
2021-02-26 02:05:00 +0500

    добавил вызываемые функции

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from src.db.db import Db
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_rating(user_id):
+    db = Db()
+    quiz = db.get_quiz(user_id)
+    score_quiz = quiz["score"]
+    max_score_quiz = quiz["total_score"]
+    final_quiz = db.get_final_quiz(user_id)
+    score_final_quiz = final_quiz["score"]
+    max_score_final_quiz = 0
+    if final_quiz["exam"]:
+        max_score_final_quiz = final_quiz["total_score"]
+
+    return score_quiz + score_final_quiz + max_score_final_quiz + max_score_quiz
 
 
-# Press the green button in the gutter to run the script.
+def init():
+    """
+    Здесь мы создаем таблицу с user_id и rating
+    Проходим по всем пользователям, делаем проверку доступа, вычисляем рейтинг, добавляем в таблицу
+    """
+    db = Db()
+    try:
+        db.create_rating_table()
+        db.create_content_table()
+    except:
+        print("db probably exist")
+    users = db.get_users()
+    for user_id in users:
+        db.get_new_stats(user_id)
+    for user_id in users:
+        rating = get_rating(user_id)
+        db.add_user_rating(user_id, rating)
+    return 0
+
+
+def main(user_id):
+    """
+    Делаем запрос в таблицу rating. Если данного пользователя нет, значит он не выполнил условия.
+    """
+    db = Db()
+    user_rating = db.get_user_from_rating(user_id)
+    if user_rating:
+        print(user_rating)
+    else:
+        print("Пользователь не участвует в рейтинге")
+    return 0
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    user = 1
+    init()
+    main(user)
